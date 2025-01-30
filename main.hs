3baa5c658622876e189b1abea34de46250022b9d
IgorAugusto01 <augustoigor731@gmail.com>
2025-01-30 18:56:25 -0300

    main preenchida

--- main.hs
+++ main.hs
@@ -2,4 +2,16 @@ import Funcoes
 
 
  
+ main :: IO ()
+main = do
+  
+  -- recolhe as palvras do arquivo colocando
+ palavras <- recolherPalavras "BancoDePalavras.txt"
+
+ -- monta uma lista onde cada a partir da vírgula
+ let lista = criaLista ',' "" palavras
+ let total = contaPalavras lista
  
+ indiceAleatorio <- gerarAleatorio 0 (total - 1) 
+ let palavraSorteada = escolherPalavraPeloIndice 0 indiceAleatorio  lista
+ print palavraSorteada
