d7ec95f9206cb40393ecc774c83b83517b6234e9
paulo1O <gilsonandrade1970@gmail.com>
2025-01-31 23:11:38 -0300

    OK

--- main.hs
+++ main.hs
@@ -16,8 +16,24 @@ criarTentativasVazias x =  [tentativaVazia] ++ criarTentativasVazias (x-1)
     where
         tentativaVazia =  "_ _ _ _ _" 
 
-adicionarTentativa :: Palavra -> Jogo -> Jogo 
-adicionarTentativa tentativa (palavraCorreta, tentativas) = (palavraCorreta, tentativas ++ [tentativa])
+
+atualizarTentativas :: [Palavra] -> Palavra -> Palavra -> [Palavra]
+atualizarTentativas [] _ _ = []  
+atualizarTentativas (x:xs) palavraCerta tentativa
+    | x == "_ _ _ _ _" = verificarResultado palavraCerta palavraCerta tentativa : xs  -- Substitui a primeira tentativa vazia
+    | otherwise = x : atualizarTentativas xs palavraCerta tentativa
+
+--x = correta
+verificarResultado :: Palavra -> Palavra -> Palavra -> Palavra
+verificarResultado _ [] _ = []
+verificarResultado _ _ [] = []
+verificarResultado ts (x:xs) (y:ys) = aplicarCorLetra (compara x y, y) ++ verificarResultado ts xs ys
+  where
+    compara x y
+        | x == y = Corretas
+        | taNaLista ts y = Parcial
+        | otherwise = Errado
+
 
 taNaLista :: Eq a => [a] -> a -> Bool
 taNaLista [] _ = False
@@ -43,52 +59,33 @@ aplicarCor Parcial letra = "\x1b[33m" ++ [letra] ++ "\x1b[0m" -- amarelo
 
 aplicarCorLetra :: (Resultado, Char) -> String
 aplicarCorLetra (resultado,letra) = aplicarCor resultado letra
---x = correta
-verificarResultado :: Palavra -> Palavra -> Palavra -> Palavra
-verificarResultado _ [] _ = []
-verificarResultado _ _ [] = []
-verificarResultado ts (x:xs) (y:ys) = aplicarCorLetra (compara x y, y) ++ verificarResultado ts xs ys
+
+imprimirLista :: [Palavra] -> IO ()
+imprimirLista xs = mapM_ putStrLn [x | x <- xs]
+
+jogar :: Palavra -> [Palavra] -> IO ()
+jogar palavraCerta listaTentativas = loop 6 listaTentativas
   where
-    compara x y
-        | x == y = Corretas
-        | taNaLista ts y = Parcial
-        | otherwise = Errado
-
-imprimirLista :: [Palavra] -> IO()
-imprimirLista [] = return ()       
-imprimirLista (x:xs) = do
-    putStrLn x                     
-    imprimirLista xs 
-
-jogar :: Palavra -> [Palavra] -> IO()
-jogar palavraCerta listaTentativas = loop 6 
-    where
-        loop 0  = putStrLn ("Perdeu!!! A palavra correta é: " ++ palavraCerta)
-        loop n = do
-            imprimirLista listaTentativas
-            putStrLn ("Digite sua tentativa:")
-            tentativaUsuario <- getLine
-            if(tamanhoPalavra tentativaUsuario /= tamanhoPalavra palavraCerta)
-                then do
-                    putStrLn ("A palavra tem " ++ show (tamanhoPalavra palavraCerta))
-                    loop n
-                else do
-                    --verificação com cor
-                    let contLetraCerta = verificarPalavra palavraCerta tentativaUsuario
-                    let palavraColorida = verificarResultado palavraCerta palavraCerta tentativaUsuario
-                    --adicionarTentativa tentativaUsuario
-                    putStrLn ("Tentativa: " ++ show contLetraCerta)
-                    putStrLn ("Tentativa: " ++  palavraColorida)
-                    if(tentativaUsuario == palavraCerta)
-                        then putStrLn "Acertou"
-                        else
-                            loop (n-1)        
+    loop 0 _ = putStrLn ("Perdeu!!! A palavra correta é: " ++ palavraCerta)
+    loop n tentativas = do
+        imprimirLista tentativas
+        putStrLn "Digite sua tentativa:"
+        tentativaUsuario <- getLine
+        if tamanhoPalavra tentativaUsuario /= tamanhoPalavra palavraCerta
+            then do
+                putStrLn ("A palavra tem " ++ show (tamanhoPalavra palavraCerta))
+                loop n tentativas
+            else do
+                let novasTentativas = atualizarTentativas tentativas palavraCerta tentativaUsuario
+                putStrLn ("Tentativa: " ++ verificarResultado palavraCerta palavraCerta tentativaUsuario)
+                if tentativaUsuario == palavraCerta
+                    then putStrLn "Acertou!"
+                    else loop (n-1) novasTentativas
 
 main :: IO ()
 main = do
- palavras <- recolherPalavras "BancoDePalavras.txt"
- let lista = criaLista ',' "" palavras
- --print lista
- jogar "nação" (criarTentativasVazias 6)
+    palavras <- recolherPalavras "BancoDePalavras.txt"
+    let lista = criaLista ',' "" palavras
+    jogar "nação" (criarTentativasVazias 6)
  
  
